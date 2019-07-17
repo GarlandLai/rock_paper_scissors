1b48fe3e83ff53868cffc4329a06fecb1b5b6b18
Garland Lai and Mike Lofgren <mike@>
2019-07-17 11:27:34 -0700

    fix logic.

--- lib/rps.rb
+++ lib/rps.rb
@@ -13,15 +13,15 @@ class RPS
     @@computer_move = rand(1..3)#Gives us a random value
     @@computer_value = @@moves.fetch(@@computer_move)#This changes the value
 
-   puts "You threw #{@@player_move} Computer threw #{@@computer_value}"
     if @@player_move == "SCISSORS" && @@computer_value == "ROCK" ||  @@player_move == "ROCK" && @@computer_value == "PAPER" || @@player_move == "PAPER" && @@computer_value == "SCISSORS"
-      puts "You lose!"
+      puts "You threw #{@@player_move.downcase} Computer threw #{@@computer_value.downcase}. You lose!"
     elsif @@player_move == "SCISSORS" && @@computer_value == "PAPER" ||  @@player_move == "ROCK" && @@computer_value == "SCISSORS" || @@player_move == "PAPER" && @@computer_value == "ROCK"
-      puts "You win!"
-    elsif @@player_move == "SCISSORS" && @@computer_value == "SCISSORS" || @@player_move == "ROCK" && @@computer_value == "ROCK" || @@player_move == "PAPER" && @@computer_value == "PAPER"
-      puts "It's a tie!"
+      puts "You threw #{@@player_move.downcase} Computer threw #{@@computer_value.downcase}. You win!"
+    elsif @@player_move == @@computer_value
+      puts "You threw #{@@player_move.downcase} Computer threw #{@@computer_value.downcase}. It's a tie!"
+    else
+      puts "Please enter a valid move!"
     end
-
   end
 end
 
@@ -29,25 +29,3 @@ puts "Please choose rock, paper, or scissors."
 input = gets.chomp
 game = RPS.new(input)
 puts game.game_calc
-
-
-
-# old logic that gives values
-
-# require('pry')
-#
-# class String
-#   def game_calc
-#     moves = {"SCISSOR" => 1,"ROCK" => 2,"PAPER" => 3}
-#     player_move = self.upcase()#This takes the input move and capitolizes
-#     player_value = moves.fetch(player_move)#This changes the value
-#     computer_move = rand(1..3)#Gives us a random value
-#
-#     binding.pry
-#
-#   end
-# end
-#
-# puts "Please choose rock, paper, or scissor."
-# input = gets.chomp
-# puts input.game_calc()
